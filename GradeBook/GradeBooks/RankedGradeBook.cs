dfec70f74d2a8b148f8984e99224a6dcf46d9da4
Samuel Lee <samwblee@gmail.com>
2020-06-10 21:15:05 -0400

    Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,5 +7,20 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade){
+            if(Students.Count < 5){
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students.");
+            }
+            if(averageGrade > 80)
+                return 'A';
+            if(averageGrade > 60)
+                return 'B';
+            if(averageGrade > 40)
+                return 'C';
+            if(averageGrade > 20)
+                return 'D';
+            return 'F';
+        }
     }
 }
